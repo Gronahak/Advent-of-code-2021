e4e4facf9b804d67911438a899a7a6c332157194
hugo <hugo@findizer.fr>
2022-01-17 21:49:39 +0100

    [FEAT] day 15 first draft

--- DAY_15/day_15.py
+++ DAY_15/day_15.py
@@ -2,13 +2,35 @@
 DEBUG_MODE = False
 DEBUG_MODE = True
 
-class Foo():
+
+class Chiton
+    def __init__(self, risk_level):
+        self.risk_level = risk_level
+        self.neighbors = []
+
+    def connect(self, other):
+        self.neighbors.append(other)
+        other.neighbors.append(self)
+
+class Cavern():
     def __init__(self):
         pass
 
     def parse(self, f):
+        previous_row = []
+        current_row = []
         for l in f.readlines():
+            previous_chiton = None
             l = l.strip()
+
+            for risk_level in l:
+                c = Chiton(int(risk_level))
+                current_row.append(c)
+
+                if not previous_chiton:
+                    previous_chiton = c
+                else:
+                    c.connect(previous_chiton)
 
     def show_answer_p1(self):
         pass
@@ -17,24 +39,24 @@ def part_1():
     debug = '_debug_part1' if DEBUG_MODE else ''
     path_to_file = f'DAY_15/input{debug}.txt'
 
-    foo = Foo()
+    chiton = Cavern()
 
     with open(path_to_file, 'r') as f:
-        foo.parse(f)
+        chiton.parse(f)
 
-    foo.show_answer_p1()
+    chiton.show_answer_p1()
 
 
 def part_2():
     debug = '_debug_part1' if DEBUG_MODE else ''
     path_to_file = f'DAY_15/input{debug}.txt'
 
-    foo = Foo()
+    chiton = Cavern()
 
     with open(path_to_file, 'r') as f:
-        foo.parse(f)
+        chiton.parse(f)
         
-    foo.show_answer_p2()
+    chiton.show_answer_p2()
 
 
 
